1aae8a2a0027c3998b3df1d7bedbf091259d04ec
Michael Empunkt <gemichelst@users.noreply.github.com>
2016-03-30 03:10:47 +0200

    Added files via upload

---
+++ pyload-getPackageID.py
@@ -0,0 +1,17 @@
+from module.remote.thriftbackend.ThriftClient import ThriftClient, WrongLogin
+import sys
+
+try:
+    client = ThriftClient(host="127.0.0.1", port=7227, user="api", password="corenftpx1")
+except:
+    print "Login was wrong"
+    exit()
+
+#define packageID as argument
+fid=int(sys.argv[1])
+
+#print packageName    
+def PackageID(n):
+    data = client.getFileData(n)
+    print data.packageID
+PackageID(fid)
---
+++ pyload-getPackageName.py
@@ -0,0 +1,17 @@
+from module.remote.thriftbackend.ThriftClient import ThriftClient, WrongLogin
+import sys
+
+try:
+    client = ThriftClient(host="127.0.0.1", port=7227, user="api", password="corenftpx1")
+except:
+    print "Login was wrong"
+    exit()
+
+#define packageID as argument
+pid=int(sys.argv[1])
+
+#print packageName    
+def PackageName(n):
+    data = client.getPackageData(n)
+    print data.name
+PackageName(pid)
